28322fc38b88d85b10a6ed104323a5cbb28b8df8
Sergei O. Udalov <sergei.udalov@gmail.com>
2020-10-31 15:15:05 +0300

    patch_14 15:15

--- model/spec/gosduma/member_spec.rb
+++ model/spec/gosduma/member_spec.rb
@@ -5,50 +5,21 @@ require "gosduma/vote"
 module Gosduma
   RSpec.describe Member do
     let(:member) { Member.new id: 1 }
-    let(:duma) { double(:duma, vote_stats: vote_stats) }
-    before { Container.stub("gateways.json", duma) }
 
-    describe "#presence" do
-      subject { member.presence }
+    before { Container.stub("json", json) }
+    let(:json) { double(:json, get: response) }
 
-      context do
-        let(:vote_stats) do
-          {
-            absentCount: 0,
-            abstainCount: 1,
-            againstCount: 1,
-            forCount: 1
-          }
-        end
-
-        it { is_expected.to eq 1 }
+    context do
+      let(:response) do
+        {
+          absentCount: 0,
+          abstainCount: 0,
+          againstCount: 0,
+          forCount: 1
+        }
       end
 
-      context do
-        let(:vote_stats) do
-          {
-            absentCount: 1,
-            abstainCount: 1,
-            againstCount: 0,
-            forCount: 0
-          }
-        end
-
-        it { is_expected.to eq 0.5 }
-      end
-
-      context do
-        let(:vote_stats) do
-          {
-            absentCount: 0,
-            abstainCount: 0,
-            againstCount: 0,
-            forCount: 0
-          }
-        end
-
-        it { is_expected.to eq 1 }
-      end
+      it { expect(member.presence).to eq 1 }
     end
   end
 end
