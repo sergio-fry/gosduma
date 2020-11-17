342d389b102cd4c87dbec1a9a66c33d202a25a77
Sergei O. Udalov <sergei.udalov@gmail.com>
2020-11-17 19:28:15 +0300

    patch_03 19:28

--- model/spec/gosduma_spec.rb
+++
@@ -1,5 +0,0 @@
-RSpec.describe Gosduma do
-  it "has a version number" do
-    expect(Gosduma::VERSION).not_to be nil
-  end
-end
--- model/spec/support/container.rb
+++
@@ -1,9 +0,0 @@
-require "dry/container/stub"
-
-Gosduma::Container.enable_stubs!
-
-RSpec.configure do |config|
-  config.after do
-    Gosduma::Container.unstub
-  end
-end
